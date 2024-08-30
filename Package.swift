3b4c304d6236cbdfb484dc9ae5aaed99916df6d5
Alex Ehlke <alex.ehlke@gmail.com>
2024-08-30 00:16:07 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "SwiftUIDownloads",
+            type: .dynamic,
             targets: ["SwiftUIDownloads"]),
     ],
     dependencies: [
