ead9bde8516b121d9944dcb2082575ca4ab88a5d
Alex Ehlke <alex.ehlke@gmail.com>
2026-04-08 03:52:10 -0400

    Refine anki sync backport updates

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIDownloads",
-    platforms: [.macOS(.v12), .iOS(.v15)],
+    platforms: [.macOS("15.0"), .iOS(.v15)],
     products: [
         .library(
             name: "SwiftUIDownloads",
